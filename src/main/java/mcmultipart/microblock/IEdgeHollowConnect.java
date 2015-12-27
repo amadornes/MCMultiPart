087f5a036cd731b89335511f3e2b51b8bb94af1f
Amadornes <amadornes@gmail.com>
2015-12-27 17:12:24 +0100

    Added ISideHollowConnect and IEdgeHollowConnect for hollow cover support

---
+++ src/main/java/mcmultipart/microblock/IEdgeHollowConnect.java
@@ -0,0 +1,13 @@
+package mcmultipart.microblock;
+
+import mcmultipart.multipart.IMultipart;
+import mcmultipart.multipart.PartSlot;
+
+
+public interface IEdgeHollowConnect extends IMultipart {
+
+    public int getHollowWidth(PartSlot slot);
+
+    public int getHollowHeight(PartSlot slot);
+
+}
---
+++ src/main/java/mcmultipart/microblock/ISideHollowConnect.java
@@ -0,0 +1,10 @@
+package mcmultipart.microblock;
+
+import mcmultipart.multipart.IMultipart;
+import net.minecraft.util.EnumFacing;
+
+public interface ISideHollowConnect extends IMultipart {
+
+    public int getHollowSize(EnumFacing side);
+
+}
