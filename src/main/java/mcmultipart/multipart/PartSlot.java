8c267f470e622d6cb1400d1a6027b7516031ecdb
Amadornes <amadornes@gmail.com>
2015-12-28 23:09:49 +0100

    Fixed PartSlot

--- src/main/java/mcmultipart/multipart/PartSlot.java
+++ src/main/java/mcmultipart/multipart/PartSlot.java
@@ -34,6 +34,11 @@ public enum PartSlot {
     CORNER_PPP(EnumFacing.SOUTH, EnumFacing.UP, EnumFacing.EAST);
 
     public static final PartSlot[] VALUES = values();
+    public static final PartSlot[] FACES = { DOWN, UP, NORTH, SOUTH, WEST, EAST };
+    public static final PartSlot[] EDGES = { EDGE_XNN, EDGE_XNP, EDGE_XPN, EDGE_XPP, EDGE_NYN, EDGE_NYP, EDGE_PYN, EDGE_PYP, EDGE_NNZ,
+            EDGE_NPZ, EDGE_PNZ, EDGE_PPZ };
+    public static final PartSlot[] CORNERS = { CORNER_NNN, CORNER_NNP, CORNER_NPN, CORNER_NPP, CORNER_PNN, CORNER_PNP, CORNER_PPN,
+            CORNER_PPP };
 
     public static PartSlot getFaceSlot(EnumFacing facing) {
 
@@ -47,14 +52,14 @@ public enum PartSlot {
         if (facing1 == facing2 || facing1.getOpposite() == facing2)
             throw new IllegalArgumentException("Tried to form an illegal edge between " + facing1 + " and " + facing2);
 
-        int x = facing1.getFrontOffsetX() + facing2.getFrontOffsetX();
+        int x = facing1.getFrontOffsetZ() + facing2.getFrontOffsetZ();
         int y = facing1.getFrontOffsetY() + facing2.getFrontOffsetY();
-        int z = facing1.getFrontOffsetZ() + facing2.getFrontOffsetZ();
+        int z = facing1.getFrontOffsetX() + facing2.getFrontOffsetX();
 
         int edge = 0;
         if (x == 0) edge = 0b0000 + (y > 0 ? 0b10 : 0b00) + (z > 0 ? 0b01 : 0b00);
-        else if (y == 0) edge = 0b0000 + (x > 0 ? 0b10 : 0b00) + (z > 0 ? 0b01 : 0b00);
-        else if (z == 0) edge = 0b0000 + (x > 0 ? 0b10 : 0b00) + (y > 0 ? 0b01 : 0b00);
+        else if (y == 0) edge = 0b0100 + (x > 0 ? 0b10 : 0b00) + (z > 0 ? 0b01 : 0b00);
+        else if (z == 0) edge = 0b1000 + (x > 0 ? 0b10 : 0b00) + (y > 0 ? 0b01 : 0b00);
 
         return VALUES[edge + 7];
     }
