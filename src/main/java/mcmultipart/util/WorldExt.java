843a3d96657723f25e5cddb07a1e5ff1cfa3730d
the_real_farfetchd <git@marco-rebhan.de>
2017-12-23 14:37:33 +0100

    Rewrote it to use MethodHandles instead of reflection

--- src/main/java/mcmultipart/util/WorldExt.java
+++ src/main/java/mcmultipart/util/WorldExt.java
@@ -1,20 +1,3 @@
-/*
- * Copyright (c) 2017 Marco Rebhan (the_real_farfetchd)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
- * documentation files (the "Software"), to deal in the Software without restriction, including without limitation
- * the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
- * to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT
- * OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
-
 package mcmultipart.util;
 
 import net.minecraft.block.state.IBlockState;
@@ -24,10 +7,11 @@ import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.storage.ExtendedBlockStorage;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
-import java.lang.reflect.Field;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 
 public class WorldExt {
-    private static Field Chunk$storageArrays = ReflectionHelper.findField(Chunk.class, "storageArrays", "field_76652_q");
+    private static MethodHandle Chunk$storageArrays = findGetter(Chunk.class, "storageArrays", "field_76652_q");
 
     /**
      * Workaround that is World#setBlockState but without calling Block#breakBlock on the old block
@@ -49,14 +33,23 @@ public class WorldExt {
             int x = pos.getX() & 15;
             int y = pos.getY() & 15;
             int z = pos.getZ() & 15;
-            ExtendedBlockStorage[] storageArrays = (ExtendedBlockStorage[]) Chunk$storageArrays.get(chunk);
+            ExtendedBlockStorage[] storageArrays = (ExtendedBlockStorage[]) Chunk$storageArrays.invoke(chunk);
             ExtendedBlockStorage storage = storageArrays[pos.getY() >> 4];
             if (storage != null) storage.set(x, y, z, state);
-        } catch (IllegalAccessException e) {
+        } catch (Throwable e) { // Chunk$storageArrays.invoke throws Throwable
             e.printStackTrace();
         }
 
         self.setBlockState(pos, state, flags);
+    }
+
+    private static MethodHandle findGetter(Class<?> clazz, String... names) {
+        try {
+            return MethodHandles.lookup().unreflectGetter(ReflectionHelper.findField(clazz, names));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private WorldExt() {}
